2650ede6519bde55437d2132b2baf029078daa65
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-29 20:29:52 -0500

    e

--- src/P5_UpperFirst.java
+++ src/P5_UpperFirst.java
@@ -10,8 +10,8 @@ public class P5_UpperFirst {
 		String superRainbow = "super rainbow";
 		String Lebron = "Lebron.James";
 
-		System.out.println(superRainbow.substring(0, 5).toUpperCase());
-		System.out.println(Lebron.substring(0, 6).toUpperCase());
+		System.out.println(superRainbow.substring(0, 5).toUpperCase() + superRainbow.substring(5, 13));
+		System.out.println(Lebron.substring(0, 6).toUpperCase() + Lebron.substring(6, 12));
 	}
 
 }
